b3f2eae78de30754de34f7db2a38d783b9a6614f
ColmeSH <mauriziocontatto@gmail.com>
2017-04-15 20:30:03 +0200

    start new lesson about dictionary like collections or objects or json

---
+++ advancedTopics/dict.py
@@ -0,0 +1,11 @@
+dict = {
+	"Name":"Maurizio",
+	"Lastname":"Bussi",
+	"age": 24,
+	"job":"Full Stack developer",
+	"preferedLanguage":"python",
+	"preferedJobPlacement":"topix",
+}
+
+print dict.items()
+
